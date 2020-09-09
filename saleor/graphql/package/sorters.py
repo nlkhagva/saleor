f7947b917b03d39c46a89cd03a988dae97ffa4af
lkhagva <lkhagva.w8@gmail.com>
2020-09-09 19:08:57 +0800

    gaduur

--- saleor/graphql/package/sorters.py
+++ saleor/graphql/package/sorters.py
@@ -4,19 +4,20 @@ from ..core.types import SortInputObjectType
 
 
 class GaduurSortField(graphene.Enum):
-    TITLE = ["name"]
-    VISIBILITY = ["is_published", "name", "url"]
-    PUBLICATION_DATE = ["publication_date", "name", "url"]
+    NAME = ["name"]
+    SHIPPING_TYPE = ["shipping_type"]
+    VISIBILITY = ["is_published", "name", "shipping_type"]
+    PUBLICATION_DATE = ["publication_date", "name", "shipping_type"]
 
     @property
     def description(self):
         if self.name in GaduurSortField.__enum__._member_names_:
             sort_name = self.name.lower().replace("_", " ")
-            return f"Sort ushops by {sort_name}."
+            return f"Sort gaduur by {sort_name}."
         raise ValueError("Unsupported enum value: %s" % self.value)
 
 
 class GaduurSortingInput(SortInputObjectType):
     class Meta:
         sort_enum = GaduurSortField
-        type_name = "ushops"
+        type_name = "gaduur"
