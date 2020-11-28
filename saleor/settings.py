77122766c4c645428ed8bec7ac11d4f54d743b88
lkhagva <lkhagva.w8@gmail.com>
2020-11-28 15:27:23 +0800

    aws RDS config

--- saleor/settings.py
+++ saleor/settings.py
@@ -63,7 +63,7 @@ INTERNAL_IPS = get_list(os.environ.get("INTERNAL_IPS", "127.0.0.1"))
 
 DATABASES = {
     "default": dj_database_url.config(
-        default="postgres://saleor:saleor@localhost:5432/unurshop_db", conn_max_age=600
+        default="postgres://masteruser:MindPoint(3)@database-2.cq7xgmoqu1pm.ap-southeast-1.rds.amazonaws.com:5432/unurshop_db", conn_max_age=600
     )
 }
 
